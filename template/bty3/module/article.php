e5331a12dd32f010dbb44327e6f6d77b5d2520e8
coryyu <723216822@qq.com>
2017-04-23 10:25:08 +0800

    提交教程最新咨询 展示

--- template/bty3/module/article.php
+++ template/bty3/module/article.php
@@ -3,9 +3,26 @@
 if(!defined('IN_T')){
 	die('hacking attempt');
 }
+//最新咨询
+$res = get_article();
+foreach($res as $k=>$v){
+	$res[$k]['tags'] = rtrim(substr(strip_tags($v['content']),0,600))."...";
+}
 
+//文章详情
 $aid = intval($_REQUEST['aid']);
 $a = $Db->query("select * from ".$Base->table('article')." where id=$aid","Row");
+
+
 $tp->assign('a',$a);
+$tp->assign('article',$res);
+
+
+//查询文章
+function get_article(){
+	$sql = "SELECT * FROM u_article WHERE u_article.is_nav ='left_bottom' ORDER BY u_article.createtime DESC LIMIT 10";
+	$res = $GLOBALS['Db']->query($sql);
+	return $res;
+}
 
 ?>
